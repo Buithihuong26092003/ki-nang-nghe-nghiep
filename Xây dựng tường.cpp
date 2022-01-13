d6b72938708aa87cd5c718242879caa9aca26bb7
A <21522130@gm.uit.edu.vn>
2022-01-13 21:28:43 +0700

    Xây dựng tường

--- Xây dựng tường.cpp
+++ Xây dựng tường.cpp
@@ -1,14 +1,18 @@
-for (int x = 0; x < 100; x++)
-{
-    gotoxy(x, 0);
-    cout << "=";
-    gotoxy(x, 25);
-    cout << "=";
-}
-for (int y = 0; y < 25; y++)
-{
-    gotoxy(1, y);
-    cout << "=";
-    gotoxy(100, y);
-    cout << "=";
-}
+for ( int x=2;x<116;x++)
+    {
+        gotoxy(x,0);
+        cout << char(196);
+        gotoxy(x,29);
+        cout << char(196);
+    }
+      for ( int y=1;y<29;y++)
+        {
+        gotoxy(1,y);
+        cout << char(179);
+        gotoxy(116,y);
+        cout << char(179);
+        }
+    gotoxy(1,0); cout <<char(218);
+    gotoxy(116,0); cout <<char(191);
+    gotoxy(1,29); cout <<char(192);
+    gotoxy(116,29); cout <<char(217);
